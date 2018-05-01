2311f53a533b466b2c2869e4bc29f87f8f270f3c
Hector Canales <00136317@uca.edu.sv>
2018-05-01 15:18:32 -0600

    Labo Guia 3

---
+++ src/hotelito/Hotelito.java
@@ -0,0 +1,21 @@
+/*
+ * 
+ * 
+ * 
+ */
+package hotelito;
+
+/**
+ *
+ * @author HsCanales <00136317@uca.edu.sv>
+ */
+public class Hotelito {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
