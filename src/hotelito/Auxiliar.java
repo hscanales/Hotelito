5c92360a7c42ae5eea5332576ecf1e869b0d1c81
Hector Canales <00136317@uca.edu.sv>
2018-05-02 13:17:43 -0600

    Se agrego la clase Auxiliar para imprimir

---
+++ src/hotelito/Auxiliar.java
@@ -0,0 +1,59 @@
+/*
+ * 
+ * 
+ * 
+ */
+package hotelito;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author HsCanales <00136317@uca.edu.sv>
+ */
+public class Auxiliar {
+    
+    Auxiliar() {
+    }
+
+    ;
+
+    void print(String s) {
+        System.out.println(s);
+    }
+
+    ;
+    void print(int i) {
+        System.out.print(i);
+    }
+
+    ;
+    void print(int[] i) {
+        System.out.print(Arrays.toString(i));
+    }
+
+    void print(double d) {
+        System.out.print(d);
+    }
+
+    ;
+    void print(boolean n) {
+        System.out.print(n + "\n");
+        
+    }
+
+    void print(String[] s) {
+        System.out.print(Arrays.toString(s));
+    }
+
+    void n(int k) {
+        for (int i = 1; i <= k; ++i) {
+            System.out.print("\n");
+        }
+    }
+
+    ;
+    void n() {
+        System.out.print("\n");
+    }
+}
--- src/hotelito/Menu.java
+++ src/hotelito/Menu.java
@@ -9,6 +9,13 @@ package hotelito;
  *
  * @author HsCanales <00136317@uca.edu.sv>
  */
-public class Menu {
+public class Menu extends Auxiliar{
+
+    public Menu() {
+    }
     
+    public void start(){
+        print("Menu Hotelito v0.1"); 
+        
+    }
 }
