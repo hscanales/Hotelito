288855bfb2db221f631c5f47110bf120ab5fafcf
nitroskate <00159817@uca.edu.sv>
2018-05-04 10:28:47 -0600

    Mostrar Reservacion

--- src/hotelito/Hotel.java
+++ src/hotelito/Hotel.java
@@ -183,7 +183,7 @@ public class Hotel extends Auxiliar {
         }
         else{
             for (Reservacion most: reservaciones){
-                System.out.println(most.getHabitacion().getPiso()+most.getHabitacion().getCorrelativo());
+                System.out.println(most.getDias()+" "+most.getHabitacion().getPiso()+most.getHabitacion().getCorrelativo()+" "+most.getCliente().getDui());
             //habitacion, letra y correlativo, cant hab, dias y paquete, nombre y apellido primero
                 
             }
