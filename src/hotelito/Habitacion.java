1566b8ea08579e33c7c4fb6bb182aeb6367c8207
Vixtor61 <00198117@uca.edu.sv>
2018-05-07 14:13:25 -0600

    añadidos comentarios

--- src/hotelito/Habitacion.java
+++ src/hotelito/Habitacion.java
@@ -16,55 +16,95 @@ public class Habitacion {
     private String Piso;
     private boolean isReserved;
     private boolean isAvailable;
-    
+    /**
+     * Constructor de la clase habitacion
+     */
     public Habitacion(){
         
     }
+    /**
+     * 
+     * @param Tipo 
+     */
     
     public void setTipo(String Tipo){
         this.Tipo = Tipo;
     }
+    /**
+     * 
+     * @return 
+     */
     
     public String getTipo(){
         return Tipo;
     }
-    
+    /**
+     * 
+     * @param Costo 
+     */
     public void setCosto(double Costo){
         this.Costo = Costo;
     }
-    
+    /**
+     * 
+     * @return 
+     */
     public double getCosto(){
         return Costo;
     }
-    
+    /**
+     * 
+     * @param Correlativo 
+     */
     public void setCorrelativo(int Correlativo){
         this.Correlativo = Correlativo;
     }
-    
+    /**
+     * 
+     * @return 
+     */
     public int getCorrelativo(){
         return Correlativo;
     }
-    
+    /**
+     * 
+     * @param Piso 
+     */
     public void setPiso(String Piso){
         this.Piso = Piso;
     }
-    
+    /**
+     * 
+     * @return 
+     */
     public String getPiso(){
         return Piso;
     }
-
+/**
+ * 
+ * @param isReserved 
+ */
     public void setIsReserved(boolean isReserved) {
         this.isReserved = isReserved;
     }
-    
+   /**
+    * 
+    * @return 
+    */ 
     public boolean IsReserved() {
         return isReserved;
     }
-
+/**
+ * 
+ * @param isAvailable 
+ */
     public void setIsAvailable(boolean isAvailable) {
         this.isAvailable = isAvailable;
     }
-    
+   /**
+    * 
+    * @return 
+    */ 
         public boolean IsAvailable() {
         return isAvailable;
     }
