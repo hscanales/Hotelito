a8054a1cbe013d4045e68d09d727609423de2ba4
Vixtor61 <00198117@uca.edu.sv>
2018-05-07 13:53:46 -0600

    añadidos comentarios

--- src/hotelito/Reservacion.java
+++ src/hotelito/Reservacion.java
@@ -6,7 +6,8 @@
 package hotelito;
 
 /**
- *
+ *Clase reservacion donde se guardan los datos
+ * de la reservacion de un hotel
  * @author nitroskate
  */
 import java.util.ArrayList;
@@ -20,7 +21,13 @@ public class Reservacion extends Hotel{
     private double total;
     
     public Reservacion(){}
-    
+    /**
+     * Constructor reservacion
+     * @param dias
+     * @param habitacion
+     * @param paquete
+     * @param cliente 
+     */
     public Reservacion(int dias, Habitacion habitacion, Paquete paquete, Cliente cliente) {
         this.dias = dias;
         this.habitacion = habitacion;
@@ -31,37 +38,74 @@ public class Reservacion extends Hotel{
         this.total=(paquete.getCosto()*this.dias)+(habitacion.getCosto()*this.dias);
         
     }
+    /**
+     * funcion para mostrar los costos
+     */
     public void Prin(){
         System.out.printf(paquete.getCosto()+ " "+ habitacion.getCosto() + " total: " +total);
     }
+    /**
+     * 
+     * @return cliente 
+     */
 
     public Cliente getCliente() {
         return cliente;
     }
+    /**
+     * 
+     * @param cliente 
+     */
 
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
     }
-
+/**
+ * 
+ * @return dias 
+ */
     public int getDias() {
         return dias;
     }
+    /**
+     * 
+     * @param dias 
+     */
     public void setDias(int dias) {
         this.dias = dias;
     }
+    /**
+     * 
+     * @return habitacion 
+     */
     public Habitacion getHabitacion() {
         return habitacion;
     }
+    /**
+     * 
+     * @param habitacion 
+     */
     public void setHabitacion(Habitacion habitacion) {
         this.habitacion = habitacion;
     }
+    /**
+     * 
+     * @return paquete 
+     */
     public Paquete getPaquete() {
         return paquete;
     }
+    /**
+     * 
+     * @param paquete 
+     */
     public void setPaquete(Paquete paquete) {
         this.paquete = paquete;
     }
-
+/**
+ * 
+ * @return total 
+ */
     public double getTotal() {
         return total;
     }
@@ -69,15 +113,18 @@ public class Reservacion extends Hotel{
     public void setTotal(double total) {
         this.total = total;
     }
-   
-    
+   /**
+    * Valida que el dia indicado este
+    * dentro del limite deseado
+    * @return dia
+    */
     public int ValidarDias(){
         while(true){
             int dia;
             Scanner leer = new Scanner(System.in);
             System.out.println("Ingrese la cantidad de dias");
             dia = leer.nextInt();
-            if (dia <= 7){
+            if (dia <= 7){ //Limite de dias
                 setDias(dia);
                 return dia;
             }
